f620f505d433bef92d83c2d6aa7fc7315f7c9929
Koonayoung518 <koony000415@gmail.com>
2024-12-30 00:41:41 +0900

    :recycle: 닉네임 자동 생성 시 랜덤 숫자 추가

--- src/main/java/com/barowoori/foodpinbackend/member/command/domain/service/GenerateNicknameService.java
+++ src/main/java/com/barowoori/foodpinbackend/member/command/domain/service/GenerateNicknameService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Random;
+
 @Service
 public class GenerateNicknameService {
     private final List<String> firstLists = List.of("멋진", "빠른", "행복한", "용감한", "웃는");
@@ -13,6 +14,7 @@ public class GenerateNicknameService {
     public String generationNickname() {
         String first = firstLists.get(random.nextInt(firstLists.size()));
         String second = secondLists.get(random.nextInt(secondLists.size()));
-        return first + " " + second;
+        int randomNumber = 1000 + random.nextInt(9000);
+        return first + " " + second + "#" + randomNumber;
     }
 }

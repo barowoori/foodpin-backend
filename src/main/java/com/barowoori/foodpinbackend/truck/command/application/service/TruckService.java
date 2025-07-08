1d9d0f45e104d078c84d11fd0b40e47756a95046
kymokim <kymokim@gmail.com>
2025-07-08 15:11:44 +0900

    ✨ Truck delete manager api fixed.

--- src/main/java/com/barowoori/foodpinbackend/truck/command/application/service/TruckService.java
+++ src/main/java/com/barowoori/foodpinbackend/truck/command/application/service/TruckService.java
@@ -367,7 +367,7 @@ public class TruckService {
         } else throw new CustomException(TruckErrorCode.TRUCK_OWNER_NOT_FOUND);
         Truck truck = truckMember.getTruck();
 
-        NotificationEvent.raise(new ManagerRemovedNotificationEvent(truck.getName(), managerId));
+        NotificationEvent.raise(new ManagerRemovedNotificationEvent(truck.getName(), memberId));
     }
 
     @Transactional

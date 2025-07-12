87b1d24fc9350a2dbdb60be53fd4ae185c719d3e
Koonayoung518 <koony000415@gmail.com>
2025-07-12 23:51:31 +0900

    :sparkles: email sender updated

--- src/main/java/com/barowoori/foodpinbackend/document/command/application/service/DocumentEmailService.java
+++ src/main/java/com/barowoori/foodpinbackend/document/command/application/service/DocumentEmailService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -39,18 +40,21 @@ public class DocumentEmailService {
                 return;
             }
             String to = event.getSubmissionEmail();
-            String subject = "[푸드핀] 행사 지원자 서류 제출 임시 제목";
-            String text = "임시 내용";
+            String subject = getSubject(event.getName(), truck.getName());
+            String text = getText(event.getName(), truck.getName());
+
 
             List<File> attachments = truckDocumentRepository.getDocumentManager(truck.getId())
                     .getDocuments().stream()
                     .filter(truckDocument -> event.getEventDocumentTypes().contains(truckDocument.getType()))
                     .map(truckDocument ->
                             imageManager.downloadFile(getDocumentFileUrl(truckDocument)))
+                    .filter(Objects::nonNull)
                     .toList();
-            mailService.sendMailWithAttachments(to, subject, text, attachments);
+            mailService.sendTruckDocumentMailWithAttachments(truck.getName(), to, subject, text, attachments);
+            log.info("행사 지원 시 이메일 발송 성공 : eventId : {}, truckId : {} ", event.getId(), truck.getId());
         } catch (Exception e) {
-            log.info("행사 지원 시 이메일 발송 실패 : " + e.getMessage());
+            log.info("행사 지원 시 이메일 발송 실패 : eventId : {}, truckId : {} " + e.getMessage(), event.getId(), truck.getId());
         }
     }
 
@@ -63,28 +67,60 @@ public class DocumentEmailService {
                 return;
             }
             String to = event.getSubmissionEmail();
-            String subject = "[푸드핀] 행사 선정된 푸드트럭 임시 제목";
-            String text = "임시 내용";
+            String subject = getSubject(event.getName(), truck.getName());
+            String text = getText(event.getName(), truck.getName());
 
             List<File> attachments = truckDocumentRepository.getDocumentManager(truck.getId())
                     .getDocuments().stream()
                     .filter(truckDocument -> event.getEventDocumentTypes().contains(truckDocument.getType()))
                     .map(truckDocument ->
                             imageManager.downloadFile(getDocumentFileUrl(truckDocument)))
+                    .filter(Objects::nonNull)
                     .toList();
-            mailService.sendMailWithAttachments(to, subject, text, attachments);
+            mailService.sendTruckDocumentMailWithAttachments(truck.getName(), to, subject, text, attachments);
 
-            log.info("행사 선정시 이메일 전송 성공 :" + to + " " + subject);
+            log.info("행사 선정 시 이메일 발송 성공 : eventId : {}, truckId : {} ", event.getId(), truck.getId());
         } catch (Exception e) {
-            log.info("행사 선정 시 이메일 발송 실패 : " + e.getMessage());
+            log.info("행사 선정 시 이메일 발송 실패 : eventId : {}, truckId : {} " + e.getMessage(), event.getId(), truck.getId());
         }
     }
 
     private String getDocumentFileUrl(TruckDocument truckDocument) {
-        return truckDocument.getPhotos().stream().findFirst()
-                .map(TruckDocumentPhoto::getFile)
-                .map(com.barowoori.foodpinbackend.file.command.domain.model.File::getPath)
-                .orElse(null);
+        com.barowoori.foodpinbackend.file.command.domain.model.File file = truckDocument.getPhotos().stream().findFirst()
+                .map(TruckDocumentPhoto::getFile).orElse(null);
+        if (file == null){
+            return null;
+        }
+        return file.getPath();
+    }
+
+    private String getSubject(String eventName, String truckName) {
+        String subject = "[{행사명}] {푸드트럭명}님의 제출 서류입니다.";
+
+        subject = subject.replace("{행사명}", eventName);
+        subject = subject.replace("{푸드트럭명}", truckName);
+
+        return subject;
+    }
+
+    private String getText(String eventName, String truckName) {
+
+        String text = "안녕하세요. 푸드핀입니다.\n" +
+                "\n" +
+                "’{행사명}’에 지원한 {푸드트럭명}의 제출서류 전달드립니다.\n" +
+                "\n" +
+                "해당 서류는 행사 공고 등록 시 설정한 제출 조건에 따라 발송되었습니다.\n" +
+                "자세한 지원 정보는 App에서 확인해 주세요.\n" +
+                "\n" +
+                "감사합니다.\n" +
+                "\n" +
+                "※ 본 메일은 발신 전용입니다.\n" +
+                "문의사항은 App > 마이페이지 > 설정 > 고객센터를 이용해 주세요.";
+
+        text = text.replace("{행사명}", eventName);
+        text = text.replace("{푸드트럭명}", truckName);
+
+        return text;
     }
 
 }

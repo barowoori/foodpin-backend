050f57fdaec8e1f15adcb79064d5b5233d1b1daa
kymokim <kymokim@gmail.com>
2025-08-27 19:27:49 +0900

    ✨ find by social query changed.

--- src/main/java/com/barowoori/foodpinbackend/member/command/domain/repository/MemberRepository.java
+++ src/main/java/com/barowoori/foodpinbackend/member/command/domain/repository/MemberRepository.java
@@ -1,12 +1,12 @@
 package com.barowoori.foodpinbackend.member.command.domain.repository;
 
 import com.barowoori.foodpinbackend.member.command.domain.model.Member;
-import com.barowoori.foodpinbackend.member.command.domain.model.SocialLoginInfo;
+import com.barowoori.foodpinbackend.member.command.domain.model.SocialLoginType;
 import com.barowoori.foodpinbackend.member.command.domain.repository.querydsl.MemberRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String>, MemberRepositoryCustom {
-    Member findBySocialLoginInfo(SocialLoginInfo socialLoginInfo);
+    Member findBySocialLoginInfo_TypeAndSocialLoginInfo_Id(SocialLoginType type, String id);
     Member findByNickname(String nickname);
     Member findByPhone(String phone);
 }

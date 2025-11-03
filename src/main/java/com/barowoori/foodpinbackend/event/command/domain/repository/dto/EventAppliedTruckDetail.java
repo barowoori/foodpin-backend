d0fc4ea2a741cea237157e4ca7ded8a034cbfb51
Koonayoung518 <koony000415@gmail.com>
2025-11-03 22:34:52 +0900

    :sparkles: EventAppliedTruckDetail eventId updated

--- src/main/java/com/barowoori/foodpinbackend/event/command/domain/repository/dto/EventAppliedTruckDetail.java
+++ src/main/java/com/barowoori/foodpinbackend/event/command/domain/repository/dto/EventAppliedTruckDetail.java
@@ -13,10 +13,12 @@ import com.barowoori.foodpinbackend.truck.command.domain.model.TruckManagerRole;
 import com.barowoori.foodpinbackend.truck.command.domain.model.TruckMenu;
 import com.barowoori.foodpinbackend.truck.command.domain.repository.dto.TruckDetail;
 import com.barowoori.foodpinbackend.truck.command.domain.repository.dto.TruckDocumentManager;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Comparator;
 import java.util.List;
 
@@ -26,7 +28,7 @@ public class EventAppliedTruckDetail extends TruckDetail {
     private String eventApplicationId;
     private String eventId;
     private EventApplicationStatus status;
-    private List<LocalDate> dates;
+    private List<EventDetail.EventDateInfo> dates;
 
     public static EventAppliedTruckDetail of(EventApplication eventApplication, Truck truck, TruckDocumentManager truckDocumentManager, List<RegionCode> regions, String regionList, List<Category> categories, List<TruckMenu> truckMenus, ImageManager imageManager) {
         return EventAppliedTruckDetail.builder()
@@ -43,9 +45,27 @@ public class EventAppliedTruckDetail extends TruckDetail {
                 .eventApplicationId(eventApplication.getId())
                 .eventId(eventApplication.getEvent().getId())
                 .status(eventApplication.getStatus())
-                .dates(eventApplication.getDates().stream()
-                        .map(EventApplicationDate::getEventDate).map(EventDate::getDate).toList())
+                .dates(eventApplication.getSortedEventDates().stream()
+                        .map(EventDateInfo::of).toList())
                 .regionList(regionList)
                 .build();
+    }
+
+    @Getter
+    @Builder
+    public static class EventDateInfo {
+        private String id;
+        private LocalDate date;
+        private LocalTime startTime;
+        private LocalTime endTime;
+
+        public static EventDetail.EventDateInfo of(EventApplicationDate eventDate) {
+            return EventDetail.EventDateInfo.builder()
+                    .id(eventDate.getId())
+                    .date(eventDate.getEventDate().getDate())
+                    .startTime(eventDate.getEventDate().getStartTime())
+                    .endTime(eventDate.getEventDate().getEndTime())
+                    .build();
+        }
     }
 }
